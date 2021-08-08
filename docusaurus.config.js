197f6f850cc3a82790a3247d0db3a46fa2832ef9
Luke Embrey <embluk@tuta.io>
2021-08-08 02:30:49 +0100

    Turn off edit URL for blogs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,9 +51,6 @@ module.exports = {
         blog: {
           showReadingTime: true,
           routeBasePath: '/blog',
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
